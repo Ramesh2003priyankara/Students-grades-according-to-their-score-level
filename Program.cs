99ac0bbe62894e8019222f1361ae889b87e1361f
Ramesh Priyankara <priyankararamesh722@gmail.com>
2025-06-23 21:14:52 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter your marks:");
+        int marks =Convert.ToInt32(Console.ReadLine());
+
+
+        if (marks >= 75)
+        {
+            Console.WriteLine("Grade A");
+        }
+        else if (65 <= marks)
+        {
+            Console.WriteLine("Grade B");
+        }
+        else if (marks >= 55)
+        {
+            Console.WriteLine("Grade c");
+        }
+        else if (marks >= 35)
+        {
+            Console.WriteLine("Grade S");
+        }
+        else 
+        {
+            Console.WriteLine("Garde W");
+        }
+
+
+    }
+}
